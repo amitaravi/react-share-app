2741af2aba658e549b183cd23d33947f35561a76
amitaravi <53140143+amitaravi@users.noreply.github.com>
2023-03-23 14:43:03 +0800

    Add files via upload

--- screens/Login.js
+++ screens/Login.js
@@ -1,76 +1,49 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
-import firebase from '../firebase';
+import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import firebase from '../services/firebase';
 
-const LoginScreen = ({ navigation }) => {
+const LoginScreen = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const navigation = useNavigation();
 
   const handleLogin = () => {
     firebase
       .auth()
       .signInWithEmailAndPassword(email, password)
       .then(() => {
-        navigation.navigate('Home');
+        navigation.navigate('UserProducts');
       })
-      .catch(error => {
+      .catch((error) => {
         console.log(error);
       });
   };
 
+  const handleRegister = () => {
+    navigation.navigate('Register');
+  };
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Login</Text>
+    <View>
+      <Text>Email</Text>
       <TextInput
-        style={styles.input}
-        placeholder="Email"
         value={email}
         onChangeText={setEmail}
-        autoCapitalize="none"
       />
+      <Text>Password</Text>
       <TextInput
-        style={styles.input}
-        placeholder="Password"
         value={password}
         onChangeText={setPassword}
-        secureTextEntry
       />
-      <TouchableOpacity style={styles.button} onPress={handleLogin}>
-        <Text style={styles.buttonText}>Login</Text>
+      <TouchableOpacity onPress={handleLogin}>
+        <Text>Login</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={handleRegister}>
+        <Text>Register</Text>
       </TouchableOpacity>
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#fff',
-  },
-  title: {
-    fontSize: 24,
-    marginBottom: 16,
-  },
-  input: {
-    height: 40,
-    width: '90%',
-    borderColor: '#ccc',
-    borderWidth: 1,
-    borderRadius: 5,
-    padding: 10,
-    marginBottom: 10,
-  },
-  button: {
-    backgroundColor: 'blue',
-    borderRadius: 5,
-    padding: 10,
-  },
-  buttonText: {
-    color: '#fff',
-    fontSize: 16,
-  },
-});
 
 export default LoginScreen;
