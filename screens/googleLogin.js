a8e19bff0ded63581d50371a5969e76ef3ab21ed
Eva <evaxlim@gmail.com>
2023-04-08 01:37:45 +0800

    updated login
    login now through google provider

---
+++ screens/googleLogin.js
@@ -0,0 +1,102 @@
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { GoogleSignin, GoogleSigninButton, statusCodes, } from '@react-native-google-signin/google-signin';
+import { auth, provider } from '../index';
+import { GoogleAuthProvider, signInWithCredential  } from 'firebase/auth';
+import Icon from 'react-native-vector-icons/Entypo';
+
+
+const GoogleLogin = ({ navigation }) => {
+    useEffect(() => {
+        GoogleSignin.configure({
+          webClientId: '151797924443-qn35poqtbktt4hmc2gl8k4kjhu2avuf4.apps.googleusercontent.com',
+        });
+      }, []);
+
+    const handleLogin = async () => {
+        try {
+            await GoogleSignin.hasPlayServices();
+            const {accessToken, idToken} = await GoogleSignin.signIn();
+            console.log("okay here");
+            const credential = GoogleAuthProvider.credential(
+              idToken,
+              accessToken,
+            );
+            console.log("okay here 2");
+            await signInWithCredential(auth, credential);
+        } catch (error) {
+          console.log(error);
+        }
+      };
+
+  return (
+    <View style={styles.container}>
+      <Icon name="shopping-basket" size={100} color="#FFFFFF"/>
+      <View style={{flexDirection: 'row'}}>
+      <Text style={styles.firstTitle}>NTU</Text>
+      <Text style={styles.secondTitle}>Share</Text>
+      </View>
+      <TouchableOpacity style={styles.button} onPress={handleLogin}>
+        <Text style={styles.buttonText}>Login</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#BCA0DC',
+  },
+  firstTitle: {
+    fontSize: 34,
+    marginBottom: 16,
+    color:"#FFFFFF",
+    fontWeight: "500"
+     },
+  secondTitle: {
+    fontSize: 34,
+    marginBottom: 16,
+    marginLeft: 10,
+    color: "#1E1E1E",
+    fontWeight: "800"
+  },
+
+  input: {
+    height: 40,
+    width: '90%',
+    borderColor: '#1E1E1E',
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10,
+  },
+  button: {
+    backgroundColor: '#1E1E1E',
+    borderRadius: 5,
+    padding: 10,
+    marginTop: 10,
+    width: '30%',
+    alignItems: 'center'
+  },
+  registerButton: {
+    backgroundColor: 'green',
+    borderRadius: 5,
+    padding: 10,
+    marginTop: 10,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: '600'
+  },
+  newUserBtn:{
+    marginTop: 15,
+    fontSize: 20,
+    color: '#1E1E1E'
+  }
+});
+
+export default GoogleLogin;
--- src/navigation/authStack.js
+++ src/navigation/authStack.js
@@ -1,5 +1,6 @@
 import { DefaultTheme, NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import GoogleLogin from "../../screens/googleLogin";
 
 import LoginScreen from "../../screens/Login";
 import Register from "../../screens/Register";
@@ -13,7 +14,7 @@ const AuthStack = () => {
     return(
         <NavigationContainer>
             <Stack.Navigator>
-               <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
+               <Stack.Screen name="Login" component={GoogleLogin} options={{ headerShown: false }} />
                <Stack.Screen name="Register" component={Register} options={{ headerShown: false }} />
             </Stack.Navigator>
         </NavigationContainer>
