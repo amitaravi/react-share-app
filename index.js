f4486ffcd1f26dd7de3e04ff077dee4dd4236898
Eva <evaxlim@gmail.com>
2023-04-08 04:49:19 +0800

    config files for google login
    updated config files for google login

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import {initializeApp} from 'firebase/app';
-import {getAuth} from 'firebase/auth';
+import {getAuth, GoogleAuthProvider} from 'firebase/auth';
 import { getFirestore, initializeFirestore } from 'firebase/firestore';
 import 'firebase/database';
 
@@ -26,8 +26,9 @@ const auth = getAuth(app);
 const db = initializeFirestore(app, {
   experimentalForceLongPolling: true
 });
+const provider = new GoogleAuthProvider();
 
-export{db, auth}; 
+export{db, auth, provider}; 
 
 AppRegistry.registerComponent(appName, () => App);
 
